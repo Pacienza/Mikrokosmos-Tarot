ff615f6045e57c54e0b6ad73dc6614b682fd5f9a
Lucas Pacienza <lucaspacienza55@gmail.com>
2024-03-28 19:24:04 -0300

    finalização do script

--- main.py
+++ main.py
@@ -12,33 +12,36 @@ style = ttk.Style(theme="vapor")
 title_label = ttk.Label(root, text="Mikrokosmos - Tarot Digital", font=("Arial", 30))
 title_label.pack(pady=10)
 
-footer_label = ttk.Label(root, text="Desenvolvido por Lucas Pacienza. 2024 \nTodos direitos reservados a VL Info®",
-                         font=("Arial", 10))
+footer_label = ttk.Label(
+    root,
+    text="Desenvolvido por Lucas Pacienza. 2024 \nTodos direitos reservados a VL Info®",
+    font=("Arial", 10),
+)
 footer_label.pack(side=tk.BOTTOM, pady=10)
 
 arcanosMaiores = {
-    'O Louco': 'img/major_arcana_fool (Personalizado).png',
-    'O Mago': 'img/major_arcana_magician (Personalizado).png',
-    'A Sacerdotisa': 'img/major_arcana_priestess (Personalizado).png',
-    'A Imperatriz': 'img/major_arcana_empress (Personalizado).png',
-    'O Imperador': 'img/major_arcana_emperor (Personalizado).png',
-    'O Hierofante': 'img/major_arcana_hierophant (Personalizado).png',
-    'Os Enamorados': 'img/major_arcana_lovers (Personalizado).png',
-    'A Carruagem': 'img/major_arcana_chariot (Personalizado).png',
-    'A Justiça': 'img/major_arcana_justice (Personalizado).png',
-    'O Eremita': 'img/major_arcana_hermit (Personalizado).png',
-    'Roda da Fortuna': 'img/major_arcana_fortune (Personalizado).png',
-    'A Força': 'img/major_arcana_strength (Personalizado).png',
-    'O Enforcado': 'img/major_arcana_hanged (Personalizado).png',
-    'Morte': 'img/major_arcana_death (Personalizado).png',
-    'A Temperança': 'img/major_arcana_temperance (Personalizado).png',
-    'O Diabo': 'img/major_arcana_devil (Personalizado).png',
-    'A Torre': 'img/major_arcana_temperance (Personalizado).png',
-    'A Estrela': 'img/major_arcana_star (Personalizado).png',
-    'A Lua': 'img/major_arcana_moon (Personalizado).png',
-    'O Sol': 'img/major_arcana_sun (Personalizado).png',
-    'O Julgamento': 'img/major_arcana_judgement (Personalizado).png',
-    'O Mundo': 'img/major_arcana_world (Personalizado).png'
+    "O Louco": "img/major_arcana_fool (Personalizado).png",
+    "O Mago": "img/major_arcana_magician (Personalizado).png",
+    "A Sacerdotisa": "img/major_arcana_priestess (Personalizado).png",
+    "A Imperatriz": "img/major_arcana_empress (Personalizado).png",
+    "O Imperador": "img/major_arcana_emperor (Personalizado).png",
+    "O Hierofante": "img/major_arcana_hierophant (Personalizado).png",
+    "Os Enamorados": "img/major_arcana_lovers (Personalizado).png",
+    "A Carruagem": "img/major_arcana_chariot (Personalizado).png",
+    "A Justiça": "img/major_arcana_justice (Personalizado).png",
+    "O Eremita": "img/major_arcana_hermit (Personalizado).png",
+    "Roda da Fortuna": "img/major_arcana_fortune (Personalizado).png",
+    "A Força": "img/major_arcana_strength (Personalizado).png",
+    "O Enforcado": "img/major_arcana_hanged (Personalizado).png",
+    "Morte": "img/major_arcana_death (Personalizado).png",
+    "A Temperança": "img/major_arcana_temperance (Personalizado).png",
+    "O Diabo": "img/major_arcana_devil (Personalizado).png",
+    "A Torre": "img/major_arcana_temperance (Personalizado).png",
+    "A Estrela": "img/major_arcana_star (Personalizado).png",
+    "A Lua": "img/major_arcana_moon (Personalizado).png",
+    "O Sol": "img/major_arcana_sun (Personalizado).png",
+    "O Julgamento": "img/major_arcana_judgement (Personalizado).png",
+    "O Mundo": "img/major_arcana_world (Personalizado).png",
 }
 
 
@@ -54,28 +57,35 @@ def energisa_sistema():
         messagebox.showinfo("Aviso!", "A intenção não pode ser vazia")
     else:
         intencoes[user_input] = user_input
-        messagebox.showinfo("Confirmação", "Sistema energizado com a intenção, prossiga para a consulta")
+        messagebox.showinfo(
+            "Confirmação", "Sistema energizado com a intenção, prossiga para a consulta"
+        )
 
 
 input_field = ttk.Entry(root)
 input_field.pack(pady=10)
-store_button = ttk.Button(root, text="Enviar intenção para o sistema", bootstyle=LIGHT, command=energisa_sistema)
+store_button = ttk.Button(
+    root,
+    text="Enviar intenção para o sistema",
+    bootstyle=LIGHT,
+    command=energisa_sistema,
+)
 store_button.pack(side=tk.TOP, padx=10, pady=10)
 
 
 def mostra_imagens(cartas, layout=None):
     new_window = Toplevel(root)
     new_window.title("Resultado Tiragem")
-    if layout == 'templo':
+    if layout == "templo":
         positions = [(0, 0), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 2)]
-    elif layout == 'peladan':
+    elif layout == "peladan":
         positions = [(1, 0), (1, 1), (1, 2), (0, 1), (2, 1)]
     else:
         positions = [(i // 3, i % 3) for i in range(len(cartas))]
 
     for i, carta in enumerate(cartas):
         img = tk.PhotoImage(file=arcanosMaiores[carta])
-        if layout in ['templo', 'peladan'] and i < len(positions):
+        if layout in ["templo", "peladan"] and i < len(positions):
             label = ttk.Label(new_window, image=img)
             label.grid(row=positions[i][0], column=positions[i][1])
         else:
@@ -96,13 +106,13 @@ def tiragem_composta(numero_cartas):
 def templo():
     numero_cartas = 7
     resultado_tiragem = tiragem_composta(numero_cartas)
-    mostra_imagens(resultado_tiragem, 'templo')
+    mostra_imagens(resultado_tiragem, "templo")
 
 
 def peladan():
     numero_cartas = 5
     resultado_tiragem = tiragem_composta(numero_cartas)
-    mostra_imagens(resultado_tiragem, 'peladan')
+    mostra_imagens(resultado_tiragem, "peladan")
 
 
 def basico():
